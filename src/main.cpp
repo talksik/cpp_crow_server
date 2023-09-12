6b8fd48fdb1ccce2252dc9665857adcaccd1bfc8
talksik <patel.arjun50@gmail.com>
2023-09-11 17:50:55 -0700

    simple app

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
 #include "../include/crow_all.h"
+#include <iostream>
 
 int main() {
   std::cout << "Hello World!" << std::endl;
+
+  crow::SimpleApp app;
+
+  CROW_ROUTE(app, "/")([]() { return "Hello world"; });
+
+  app.port(3000).multithreaded().run();
 }
